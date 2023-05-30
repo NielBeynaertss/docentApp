f460796a1b62fda646358354044ef79bb39ddbfb
NBeynaerts <niel.beynaerts@gmail.com>
2023-05-30 11:55:51 +0200

    show teachers in table functionality done

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,14 +5,46 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/leaflet@1.7.1/dist/leaflet.css" />
     <title>@yield('title')</title>
 </head>
 <body>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#"><img src="https://www.syntra.be/wp-content/uploads/2022/07/Synta-logo-klein.png" alt="logo" height="100px"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav navbar-right">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Register</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">FAQ</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact us</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    
+    
     <div class="container">
         @yield('content')
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-D1X+RTF9/kZdFx2mgZ8X/UKZj2MumJLIVjZ5b0ISxjMK3Jn9cVWgD6yBRe0+/TB" crossorigin="anonymous"></script>
+
+        <script src="https://cdn.jsdelivr.net/npm/leaflet@1.7.1/dist/leaflet.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-D1X+RTF9/kZdFx2mgZ8X/UKZj2MumJLIVjZ5b0ISxjMK3Jn9cVWgD6yBRe0+/TB" crossorigin="anonymous"></script>
     @stack('scripts')
 </body>
 </html>
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,26 +4,67 @@
 
 @section('content')
     <div class="container">
-
-        <div class="row">
-            @component('map')
-            @endcomponent
-        </div>
+        <div id="map"></div>
 
         <h1>Teachers</h1>
 
         <div class="row">
             @foreach($teachers as $teacher)
-                <div class="col-md-4 mb-3">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $teacher->lastname }}, {{ $teacher->firstname }}</h5>
-                            <h6 class="card-text">Profession: {{ $teacher->description }}</h6>
-                            <p class="card-text">Category: {{ $teacher->category->name }}</p>
+                @if($teacher->approved == 1)
+                    <div class="col-md-4 mb-3">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $teacher->lastname }} {{ $teacher->firstname }}</h5>
+                                <h6 class="card-text">Profession: {{ $teacher->description }}</h6>
+                                <p class="card-text">Category: {{ $teacher->category->name }}</p>
+                            </div>
                         </div>
                     </div>
-                </div>
+                @endif
             @endforeach
         </div>
     </div>
+
+    <style>
+        body {
+            margin: 0;
+        }
+
+        #map {
+            width: 100%;
+            height: 300px;
+        }
+    </style>
+
+    <script src="https://cdn.jsdelivr.net/npm/leaflet@1.7.1/dist/leaflet.js"></script>
+    <script>
+        var map;
+        var ajaxRequest;
+        var plotlist;
+        var plotlayers=[];
+
+        // Init Open Street Maps
+        function initmap() {
+            // set up the map
+            map = new L.Map('map');
+            var osmUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+            var osmAttrib='Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
+            var osm = new L.TileLayer(osmUrl, {minZoom: 2, maxZoom: 19, attribution: osmAttrib});
+            map.setView(new L.LatLng(50.640280, 4.666710), 8); // Set initial view to Belgium
+            map.addLayer(osm);
+
+            // Add markers for each teacher's city
+            @foreach($teachers as $teacher)
+                @if($teacher->approved == 1 && $teacher->city && $teacher->city->codecity)
+                    var teacherCoords = [{{ $teacher->city->codecity->latitude }}, {{ $teacher->city->codecity->longitude }}];
+                    var marker = L.marker(teacherCoords).addTo(map);
+                    marker.bindPopup("<b>{{ $teacher->lastname }}, {{ $teacher->firstname }}</b><br>{{ $teacher->city->codecity->name }}").openPopup();
+                @endif
+            @endforeach
+        }
+
+        window.onload = function() {
+            initmap();
+        };
+    </script>
 @endsection
